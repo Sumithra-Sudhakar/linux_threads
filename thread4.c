96dada2fe641a6c4907351149065037fc479344d
Sumithra_Sudhakar <sumisooriya@gmail.com>
2021-04-26 14:13:42 +0530

    multiple threads

---
+++ thread4.c
@@ -0,0 +1,31 @@
+
+//including the required header files
+#include <stdio.h> 
+
+#include <unistd.h> 
+
+#include <pthread.h> 
+
+ 
+
+void * hello(void *input) { 
+
+    printf("%s\n", (char *)input); //type casting to char since we know the input is string
+
+    pthread_exit(NULL); //exiting the thread
+
+} 
+
+ 
+
+int main(void) { 
+
+    pthread_t tid; //instantiating the thread
+
+    pthread_create(&tid, NULL, hello, "hello world"); //passing a string as the last argument in the create function indicating that it is the parameter of the function jello
+
+    pthread_join(tid, NULL); //joining the thread
+
+    return 0; 
+
+}
